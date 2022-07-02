d6864260aff81a91ab3fc81fd5e77a6d279d00c3
ManikandaprabhuGunasekaran <41508446+ManikandaprabhuGunasekaran@users.noreply.github.com>
2022-07-02 09:02:48 +0530

    week3.day1.assignments

---
+++ Automation.java
@@ -0,0 +1,22 @@
+package week3.day1.assignment5;
+
+public class Automation extends MultipleLangauge
+			implements Language,TestTool {
+
+	public void Selenium() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Java() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ruby() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ MultipleLangauge.java
@@ -0,0 +1,8 @@
+package week3.day1.assignment5;
+
+public abstract class MultipleLangauge {
+
+	public void python() {}
+	
+	public abstract void ruby();
+}
